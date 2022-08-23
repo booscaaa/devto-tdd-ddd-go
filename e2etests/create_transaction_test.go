a7fb2ee28b63f2855e1e8d527b3ca812c63cb668
Vinícius Boscardin <vinicius@webcgi.com.br>
2022-08-23 17:24:35 -0300

    ✨ adicina testes iniciais de e2e

--- e2etests/create_transaction_test.go
+++ e2etests/create_transaction_test.go
@@ -1,4 +1,4 @@
-package main_test
+package e2etests_test
 
 import (
 	"bytes"
@@ -45,59 +45,46 @@ func doRequest(method, url string, payload []byte) (*http.Response, error) {
 	return response, nil
 }
 
-func createTransaction(t *testing.T, value float64, numberInstallments int64) *string {
+func createTransaction(t *testing.T, value float64, numberInstallments int64) (*string, error) {
 	payload := map[string]interface{}{
 		"type":               "CREDIT_CARD",
 		"value":              value,
 		"numberInstallments": numberInstallments,
 	}
 	responsePayload := map[string]interface{}{}
-
 	payloadJson, _ := json.Marshal(payload)
 
 	response, err := doRequest(http.MethodPost, BASE_URL+"/", payloadJson)
 
 	if err != nil {
-		t.Error(err)
-		return nil
+		return nil, err
 	}
 
-	fmt.Println(response.StatusCode)
-
-	assert.NotEqual(t, 200, response.StatusCode, "Erro")
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("Status code is not 200, its %v", response.StatusCode)
+	}
 
 	json.NewDecoder(response.Body).Decode(&responsePayload)
 	transactionID := responsePayload["transactionID"].(string)
-	return &transactionID
+	return &transactionID, nil
 }
 
-func getTransactionByID(t *testing.T, transactionID string) *Transaction {
+func getTransactionByID(t *testing.T, transactionID string) (*Transaction, error) {
 	response, err := doRequest(http.MethodGet, BASE_URL+"/transaction/"+transactionID, nil)
 	responsePayload := Transaction{}
 
 	if err != nil {
-		t.Error(err)
-		return nil
+		return nil, err
 	}
 
 	json.NewDecoder(response.Body).Decode(&responsePayload)
-	return &responsePayload
+	return &responsePayload, nil
 }
 
-func TestMain(t *testing.T) {
-	transactionID := createTransaction(t, 630.21, 12)
-	transactionCreated := getTransactionByID(t, *transactionID)
-
-	assert.NotNil(t, transactionCreated, "Ta nulo meu patrão!")
-	assert.NotZero(t, transactionCreated.Installments, "Não tem parcela nenhuma aqui!")
-	assert.Len(t, transactionCreated.Installments, 12, "Deveria ter 12 parcelas!")
-	assert.Equal(t, 52.51, transactionCreated.Installments[0].Value, "O valor da primeira parcela ta errado!")
-	assert.Equal(t, 52.6, transactionCreated.Installments[len(transactionCreated.Installments)-1].Value, "O valor da última parcela ta errado!")
-
-	sumInstallments := 0.00
-	for _, installment := range transactionCreated.Installments {
-		sumInstallments += installment.Value
-	}
-
-	assert.Equal(t, 630.21, sumInstallments, "A soma das parelas não fechou meu patrão!")
+func TestE2E(t *testing.T) {
+	t.Run("Should create a transaction returning ID", func(t *testing.T) {
+		transactionID, err := createTransaction(t, 630.21, 12)
+		assert.Nil(t, err, fmt.Sprintf("Should error return null but return %s", err.Error()))
+		assert.NotNil(t, transactionID, fmt.Sprintf("Should return transaction ID but return null"))
+	})
 }
--- main.go
+++ main.go
@@ -19,18 +19,6 @@ func init() {
 func main() {
 	router := chi.NewRouter()
 
-	router.Post("/", func(response http.ResponseWriter, request *http.Request) {
-		response.WriteHeader(http.StatusOK)
-		response.Write([]byte("HELLO WORD"))
-	})
-
-	router.Get("/{id}", func(response http.ResponseWriter, request *http.Request) {
-		id := chi.URLParam(request, "id")
-
-		response.WriteHeader(http.StatusOK)
-		response.Write([]byte(id))
-	})
-
 	serverPort := viper.GetString("server.http.port")
 	fmt.Println(serverPort)
 	http.ListenAndServe(":"+serverPort, router)
